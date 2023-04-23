ff52b2ae9b4ad6eac91d670b2b51f552a6dc838e
zachrlew <113840615+zachrlew@users.noreply.github.com>
2023-04-23 15:57:14 -0400

    Add files via upload

---
+++ Final Project/Blackjack.py
@@ -0,0 +1,113 @@
+import random
+
+# create a dictionary to store the values of each card
+card_values = {
+    'Ace': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10
+}
+
+# create a list of card suits
+card_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+# create a list to hold the deck of cards
+deck = []
+
+# create a function to create the deck of cards
+def create_deck():
+    for suit in card_suits:
+        for card, value in card_values.items():
+            deck.append((card + ' of ' + suit, value))
+
+# create a function to shuffle the deck
+def shuffle_deck():
+    random.shuffle(deck)
+
+# create a function to deal a card
+def deal_card(hand):
+    card = deck.pop()
+    hand.append(card)
+
+# create a function to calculate the value of a hand
+def calculate_hand(hand):
+    total = 0
+    aces = 0
+    for card in hand:
+        total += card[1]
+        if card[0].startswith('Ace'):
+            aces += 1
+    while total <= 11 and aces > 0:
+        total += 10
+        aces -= 1
+    return total
+
+# create a function to play the game
+def play_game():
+    print('Welcome to Blackjack!')
+    create_deck()
+    shuffle_deck()
+    player_hand = []
+    dealer_hand = []
+    deal_card(player_hand)
+    deal_card(dealer_hand)
+    deal_card(player_hand)
+    deal_card(dealer_hand)
+    print('Player hand: ', player_hand[0][0], 'and', player_hand[1][0])
+    print('Dealer hand: ', dealer_hand[0][0], 'and one face-down card')
+    player_total = calculate_hand(player_hand)
+    dealer_total = calculate_hand(dealer_hand)
+    while player_total < 21:
+        action = input('Would you like to hit or stand? ')
+        if action == 'hit':
+            deal_card(player_hand)
+            print('Player hand: ', end='')
+            for card in player_hand:
+                print(card[0], end=', ')
+            print('')
+            player_total = calculate_hand(player_hand)
+            if player_total >= 21:
+                break
+        elif action == 'stand':
+            break
+    if player_total > 21:
+        print('Bust! You lose.')
+        return
+    print('Dealer hand: ', end='')
+    for card in dealer_hand:
+        print(card[0], end=', ')
+    print('')
+    while dealer_total < 17:
+        deal_card(dealer_hand)
+        dealer_total = calculate_hand(dealer_hand)
+        print('Dealer hand: ', end='')
+        for card in dealer_hand:
+            print(card[0], end=', ')
+        print('')
+    if dealer_total > 21:
+        print('Dealer bust! You win!')
+    elif dealer_total > player_total:
+        print('Dealer wins!')
+    elif dealer_total < player_total:
+        print('You win!')
+    else:
+        print('A tie! Dealer wins!')
+
+# start the game
+while True:
+    play_game()
+    answer = input("Would you like to play again? (y/n): ")
+    if answer != 'y':
+        break
+        
+#restart the game
+replay_game()
---
+++ Final Project/readme.txt
@@ -0,0 +1,36 @@
+For this script, please make sure you have the most current version of Python installed (3.11.3 as of current writing)
+To install and/or update python, please visit https://www.python.org/downloads/ and follow the instructions for download and installation.
+
+I ran this script two different ways, both on a machine running Windows 10.  The first was using Microsoft VS (Visual Studio) Code,
+and the second was by double clicking the file where I had it saved on my computer.  Either method should work.
+
+For VS Code, please download the file and open it using VS Code, or copy the code into an open window within VS Code and select "Run."
+
+For running directly from a saved location, simply go to where you have the file saved, and double click the file.
+This method will create a pop up terminal window.
+
+Input is text based.  The game will tell you what you (the player) have for cards and will tell you the value of the Dealer's face up card,
+and that they have a face down card.  You will have to manually add the value of your cards to know your total.
+
+As mentioned, input is text based.  You will be given your initial cards and asked if you would like to hit (get another card)
+or stand (not get another card).  If you choose to hit, you will be given the option to hit or stand again unless your card value
+exceeds 21.  Following the completion of the game, you will be asked if you would like to play again (options are y/n).
+Selecting "y" will restart the game.  Selecting "n" will end the game and exit the program.
+
+For anyone unfamiliar with the game, the goal of Blackjack is to get as close as you can to a value of 21 without going over (referred to as a bust).
+If you tie with the dealer (sometimes called a push) then the dealer automatically wins as the dealer wins all ties.
+
+The values in Blackjack are as follows:
+2 = 2
+3 = 3
+4 = 4
+5 = 5
+6 = 6
+7 = 7
+8 = 8
+9 = 9
+10 = 10
+Jack = 10
+Queen = 10
+King = 10
+Ace = 1 OR 11 (this depends on your current total. For example: 3 & Ace = 14, Jack & Ace = 21 (This is also blackjack depending on the jack) BUT 8 + 3 + Ace = 12)
